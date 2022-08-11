8827f048f613e53101ef38edf5fbebfc67d2ffd2
IVAN BALABAY <i.balabai@gmail.com>
2022-08-11 16:58:11 +0300

    Task02 completed

---
+++ Seminar01/task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите любое целое число А:  ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите любое целое число B:  ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)
+    {
+        Console.WriteLine ($"A = {A}; B = {B} -> max = {A}");
+    }
+else
+    {
+        Console.WriteLine ($"A = {A}; B = {B} -> max = {B}");
+    }
