cf7898a2091055b28f552c7c508cb00fa30c8c5e
IVAN BALABAY <i.balabai@gmail.com>
2022-08-11 17:26:28 +0300

    Task04 completed

---
+++ Seminar01/task04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите первое число (любое целое):  ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число (любое целое):  ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число (любое целое):  ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if (numA > max) max = numA;
+if (numB > max) max = numB;    
+if (numC > max) max = numC;
+
+Console.WriteLine($"{numA}, {numB}, {numC} -> {max}");
