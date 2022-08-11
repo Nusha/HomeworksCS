4bf379197c75857cae8652e888004af3ec1a49c5
IVAN BALABAY <i.balabai@gmail.com>
2022-08-11 18:50:10 +0300

    Task08 completed

---
+++ Seminar01/task08/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите любое целое положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1)
+    {
+    Console.WriteLine("Необходимо ввести любое положительное число от 1");
+    }
+else {
+int num = n;
+if (n % 2 != 0)
+    {
+    n = n - 1;
+    }
+Console.Write($"{num} -> ");
+while (n >= 1)
+    {
+    Console.Write($"{n},");
+    n = n - 2;    
+    }
+Console.WriteLine();
+}
