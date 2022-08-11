05ae2734fba2a4ee0d9938366bcf92c7b8f3f690
IVAN BALABAY <i.balabai@gmail.com>
2022-08-11 17:50:26 +0300

    Task06 completed

---
+++ Seminar01/task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите любое целое число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+    {
+        Console.WriteLine($"{num} -> да");
+    }
+else
+    {
+        Console.WriteLine($"{num} -> нет");
+        }
