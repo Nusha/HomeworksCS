9494fcca6d15b12d0b2002fa114ace1e4dc2cff9
IVAN BALABAY <i.balabai@gmail.com>
2022-08-15 02:51:02 +0300

    Task10 completed

---
+++ Seminar02/task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет. 
+// Решить без использования строк.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int input = num;
+num = Math.Abs(num);
+int n3 = 0;
+while (num > 999)
+    {
+    num=num/10; 
+    }
+if (num < 100)
+    {
+        Console.WriteLine($"{input} -> третьей цифры нет");
+    }
+else
+    {
+        n3=num%10;
+        Console.WriteLine($"{input} -> {n3}");
+    }
---
+++ Seminar02/task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Write("Введите порядковый номер дня недели (от 1 до 7): ");
+int weekday = Convert.ToInt32(Console.ReadLine());
+if (weekday < 1 || weekday > 7)
+{
+    Console.WriteLine($"{weekday} вне диапазона от 1 до 7");
+}
+else if (weekday == 6 || weekday == 7)
+{
+    Console.WriteLine($"{weekday} -> да");
+}
+else
+{
+    Console.WriteLine($"{weekday} -> нет");
+}
