a6dbca158dad427ae94ebf960eebba7067048834
IVAN BALABAY <i.balabai@gmail.com>
2022-08-18 04:33:50 +0300

    task23 completed

---
+++ Seminar03/task23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.Write("Введите число от 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 1)
+{   Console.WriteLine("Число        Куб");
+
+    for (int i = 1; i <= n;)
+    {
+        Console.WriteLine($" {i}     | {i * i * i} ");
+        Console.WriteLine("_______________________");
+        i++;
+    }
+}
+else Console.WriteLine("Число меньше 1.");
