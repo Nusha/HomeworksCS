7340e7920e3f8dc95f51ca4138659d6cf12c2d31
IVAN BALABAY <i.balabai@gmail.com>
2022-08-27 19:05:58 +0300

    Task41 completed

--- Seminar05/task38/Program.cs
+++ Seminar05/task38/Program.cs
@@ -1,101 +1,148 @@
-﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+﻿// // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
 
-// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
-
-
+// // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
 
 
-double[] CreateArrayAskElements(int size)
-{
-    if (size >= 4 && size <= 20)
-    {
+
+
+// double[] CreateArrayAskElements(int size)
+// {
+//     if (size >= 4 && size <= 20)
+//     {
 
 
 
-        double[] array = new double[size];
+//         double[] array = new double[size];
 
-        for (int i = 0; i < size; i++)
-        {
-            array[i] =  Math.Round(new Random().NextDouble()*100, 1);
-        }
-        return array;
+//         for (int i = 0; i < size; i++)
+//         {
+//             array[i] =  Math.Round(new Random().NextDouble()*100, 1);
+//         }
+//         return array;
 
-    }
-    else
-    {
-        Console.WriteLine("Размер массива вне допустимого диапазона");
-        return null!;
-    }
-}
+//     }
+//     else
+//     {
+//         Console.WriteLine("Размер массива вне допустимого диапазона");
+//         return null!;
+//     }
+// }
 
-void PrintArray8(double[] arr)
-{
-    if (arr != null)
-    {
-        Console.Write("[");
-        for (int i = 0; i < arr.Length; i++)
-        {
-            Console.Write($" {arr[i]},");
-        }
-        Console.Write("]");
-    }
-    else
-    {
-        Console.WriteLine("Не переданы значения масcива для вывода на экран");
-    }
-}
+// void PrintArray8(double[] arr)
+// {
+//     if (arr != null)
+//     {
+//         Console.Write("[");
+//         for (int i = 0; i < arr.Length; i++)
+//         {
+//             Console.Write($" {arr[i]},");
+//         }
+//         Console.Write("]");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Не переданы значения масcива для вывода на экран");
+//     }
+// }
 
-object FindMinArr(double [] arr)
-{
-    if (arr != null)
-    {
-        double min = arr[0];
-        foreach (var item in arr)
-        {
-         if (item < min) min = item;  
-        }
-    return min;
-    }
-else
-    {
+// object FindMinArr(double [] arr)
+// {
+//     if (arr != null)
+//     {
+//         double min = arr[0];
+//         foreach (var item in arr)
+//         {
+//          if (item < min) min = item;  
+//         }
+//     return min;
+//     }
+// else
+//     {
         
-        Console.WriteLine("Значение массива не передано в метод");
-        return null!;
-        
-       
-    }
-
-}
-
-object FindMaxArr(double [] arr)
-{
-    if (arr != null)
-    {
-        double max = arr[0];
-        foreach (var item in arr)
-        {
-         if (item > max) max = item;  
-        }
-    return max;
-    }
-else
-    {
-        
-        Console.WriteLine("Значение массива не передано в метод");
-        return null!;
+//         Console.WriteLine("Значение массива не передано в метод");
+//         return null!;
         
        
-    }
+//     }
 
+// }
+
+// object FindMaxArr(double [] arr)
+// {
+//     if (arr != null)
+//     {
+//         double max = arr[0];
+//         foreach (var item in arr)
+//         {
+//          if (item > max) max = item;  
+//         }
+//     return max;
+//     }
+// else
+//     {
+        
+//         Console.WriteLine("Значение массива не передано в метод");
+//         return null!;
+        
+       
+//     }
+
+// }
+
+// Console.Clear();
+// Console.Write("Введите размер массива в диапазоне от 4 до 20: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// double [] array = CreateArrayAskElements(size);
+// PrintArray8(array);
+// double min = Convert.ToDouble(FindMinArr(array));
+// double max = Convert.ToDouble(FindMaxArr(array));
+// double dif = max - min;
+// if (array != null) Console.WriteLine($" -> {Math.Round(dif, 1)}");
+
+int candis = 10;
+int step = 2;
+
+int InputNumber (string text)
+{
+Console.WriteLine(text);
+int userNumberInput = Convert.ToInt32(Console.ReadLine());
+if (userNumberInput < 9 && userNumberInput > 0) return userNumberInput;
+else 
+{
+    Console.WriteLine("Не мухлюйте. Вам придется забрать 1 кофету");
+    return 1; 
+}
+}
+bool turnFirstGamer = true;
+while (candis > 0)
+{
+    if (turnFirstGamer) 
+    {
+        candis = candis - InputNumber($"Осталось {candis} конфет. Ходит первый игрок. Сколько конфет забираете? :  ");
+        turnFirstGamer = !turnFirstGamer;
+    }
+    else
+     {
+        Console.WriteLine($"Осталось {candis} конфет. Ходит второй игрок. Сколько конфет забираете? : ");
+        candis = candis - TurnOfIi (candis, step);
+        turnFirstGamer = !turnFirstGamer;
+    }
 }
 
-Console.Clear();
-Console.Write("Введите размер массива в диапазоне от 4 до 20: ");
-int size = Convert.ToInt32(Console.ReadLine());
-double [] array = CreateArrayAskElements(size);
-PrintArray8(array);
-double min = Convert.ToDouble(FindMinArr(array));
-double max = Convert.ToDouble(FindMaxArr(array));
-double dif = max - min;
-if (array != null) Console.WriteLine($" -> {Math.Round(dif, 1)}");
+if (!turnFirstGamer) Console.WriteLine("Выиграл первый игрок");
+else Console.WriteLine("Выиграл второй игрок");
+
+int TurnOfIi (int remainsInMethod, int stepInMethod)
+{
+    int result = 1;
+
+    if (stepInMethod <= remainsInMethod ) result = remainsInMethod;
+    else 
+    {
+        result = remainsInMethod % (stepInMethod + 1);
+        
+    }
+    Console.WriteLine(result);
+    return result;
+}
---
+++ Seminar06/task41/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+var m = Int32.Parse(Console.ReadLine()!);
+
+int[] CreatArrFromKeyboard(int size)
+{
+    if (size > 1)
+    {
+        int[] array = new int[size];
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"Введите значение для {i} индекса массива: ");
+            array[i] = Int32.Parse(Console.ReadLine()!);
+        }
+        return array;
+    }
+    else return null!;
+}
+
+void PrintArray8(int[] arr)
+{
+    if (arr != null)
+    {
+        Console.Write("[");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write($" {arr[i]},");
+        }
+        Console.Write("]");
+    }
+}
+
+int? CountAboveZero(int[] arr)
+{
+    if (arr != null)
+    {
+        int num = 0;
+        int? result = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            result = arr[i] > 0 ? num = num + 1 : num;
+        }
+        return result;
+    }
+    else Console.WriteLine($"Передано недопустимое значение в метод."); return null;
+}
+
+int[] array = CreatArrFromKeyboard(m);
+
+PrintArray8(array);
+
+int? res = CountAboveZero(array);
+
+if (res.HasValue) 
+{
+    Console.WriteLine($" -> {CountAboveZero(array)}");
+}
+else Console.WriteLine("Ошибка - нет значения");
+
